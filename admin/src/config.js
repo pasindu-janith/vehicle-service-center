42885c08c396bb17445cad96a9893bdb48c9445b
pasindu-janith <pjhathurusinghe14@gmail.com>
2025-08-28 17:56:36 +0530

    admin cookie issue last

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,5 +1,6 @@
 // src/config.js
-const BASE_URL = "https://vehicle-service-center-backend.onrender.com/api/v1/admin";
+// const BASE_URL = "https://vehicle-service-center-backend.onrender.com/api/v1/admin";
+const BASE_URL = "http://localhost:4000/api/v1/admin";
 const BASE_IMAGES_URL = "https://vehicle-service-center-backend.onrender.com";
 const CLIENT_BASE_URL = "https://vehicle-service-center-client.vercel.app";
 
